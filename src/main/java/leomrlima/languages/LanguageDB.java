97b25f003b4a4ad94c07032ccc38ef02b8d49786
Leonardo Lima <leomrlima@gmail.com>
2018-05-24 12:31:03 -0500

    Creates DAO object

---
+++ src/main/java/leomrlima/languages/LanguageDB.java
@@ -0,0 +1,20 @@
+package leomrlima.languages;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class LanguageDB {
+
+  private Set<Language> languages = new LinkedHashSet<>();
+
+  public boolean addLanguage(Language language) {
+    return this.languages.add(Objects.requireNonNull(language, "language can't be null"));
+  }
+  
+  public Collection<Language> getAllLanguages() {
+    return Collections.unmodifiableSet(languages);
+  }
+}
