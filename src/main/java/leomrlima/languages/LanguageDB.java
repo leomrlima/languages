571dd533612434a01a740d1498d6340cbc385cff
Leonardo Lima <leomrlima@gmail.com>
2018-05-24 13:02:31 -0500

    Adds CDI scope to the DAO

--- src/main/java/leomrlima/languages/LanguageDB.java
+++ src/main/java/leomrlima/languages/LanguageDB.java
@@ -5,7 +5,9 @@ import java.util.Collections;
 import java.util.LinkedHashSet;
 import java.util.Objects;
 import java.util.Set;
+import javax.enterprise.context.ApplicationScoped;
 
+@ApplicationScoped
 public class LanguageDB {
 
   private Set<Language> languages = new LinkedHashSet<>();
