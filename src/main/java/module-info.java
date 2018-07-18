c1c80f87963ba678c7650899d2812345f1262d1b
Leonardo Lima <leomrlima@gmail.com>
2018-07-18 10:26:16 -0300

    Insere dependências para modularizar a aplicação

---
+++ src/main/java/module-info.java
@@ -0,0 +1,23 @@
+open module leomrlima.languages {
+	
+	requires vaadin.cdi;
+	requires vaadin.client.compiled;
+	requires vaadin.themes;
+	
+	requires vaadin.server;
+	requires jsoup;
+	requires vaadin.shared;
+	
+	requires slf4j.api;
+	
+	requires cdi.api;
+	requires javax.inject;
+	
+	requires diana.core;
+	requires diana.document;
+	requires artemis.core;
+	requires artemis.document;
+	requires diana.driver.commons;
+	requires mongodb.driver;
+	
+}
